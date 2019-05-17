c736d3c4c2deee6b130f8cc9a1df35196196480e
Grace Kim <gracekim1213@gmail.com>
2019-05-17 15:31:17 -0700

    Created SwitchCharacter script that Switches player sprite by pressing 1, 2, or 3 using sprites inResources File

---
+++ Platformer/Assets/Scripts/SwitchCharacter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Swiches player sprite by pressing 1, 2, or 3
+public class SwitchCharacter : MonoBehaviour
+{
+    public Sprite rock, paper, scissor; 
+
+    private SpriteRenderer spriteRenderer;
+
+    // Set rock, paper, scissor into sprites inside Resources file
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rock = Resources.Load<Sprite>("square");
+        paper = Resources.Load<Sprite>("circle");
+        scissor = Resources.Load<Sprite>("triangle");
+
+        if (spriteRenderer.sprite == null) // player initialized to rock
+            spriteRenderer.sprite = rock; 
+    }
+
+    // Update is called once per frame, change sprites when 1, 2, or 3 pressed
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))    //if 1 pressed, display square
+            spriteRenderer.sprite = rock;
+        
+        else if (Input.GetKeyDown(KeyCode.Alpha2))   // if 2 pressed, display circle        
+            spriteRenderer.sprite = paper;
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3))  // if 3 pressed, display triangle
+            spriteRenderer.sprite = scissor;
+
+    }
+}
